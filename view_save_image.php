9333e2f78198004a9448a61f7447c393b24302c2
Amyra Athifah <74502882+amirahathifah@users.noreply.github.com>
2022-11-09 17:23:50 +0800

    Delete view_save_image.php

--- view_save_image.php
+++
@@ -1,8 +0,0 @@
-<form action="<?= site_url('etest/question/save_image') ?>" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="text" name="hide" value="bismillah">
-
-  <input type="file" name="file[]" id="file" multiple>
-  <input type="hidden" name="<?= $this->security->get_csrf_token_name() ?>" value="<?= $this->security->get_csrf_hash() ?>">
-  <button type="submit" name="submit">UPLOAD</button>
-</form>
