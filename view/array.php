f01a409ad10b407e107c1596830278f702557165
Amyra Athifah <74502882+amirahathifah@users.noreply.github.com>
2022-11-15 09:29:35 +0800

    Update array.php

--- view/array.php
+++ view/array.php
@@ -23,3 +23,32 @@ echo $arr['trx'][2]; // Accesses 3rd value in the array, prints 75
 echo $arr['orderDetails']['name']; // Prints john doe
 echo $arr['orderDetails']['productIDs'][0]; // prints 11
 echo $arr['myMethod'](); // Executes myMethod function; prints Hello, World!
+
+// Array of objects
+// Array
+// (
+//     [0] => stdClass Object
+//         (
+//             [ib_url] => https://zanko-oes-dev.s3.ap-southeast-1.amazonaws.com/question/636e146a390bd.jpg
+//             [ib_desc] => 
+//         )
+
+//     [1] => stdClass Object
+//         (
+//             [ib_url] => https://zanko-oes-dev.s3.ap-southeast-1.amazonaws.com/question/636e146c2f9eb.jpg
+//             [ib_desc] => 
+//         )
+
+//     [2] => stdClass Object
+//         (
+//             [ib_url] => https://zanko-oes-dev.s3.ap-southeast-1.amazonaws.com/question/636e1a8517b54.png
+//             [ib_desc] => 
+//         )
+// )
+for( $i=0 ; $i < $total ; $i++ )
+{
+    $ib_url = $images[$i]->ib_url;
+    $ib_desc = $images[$i]['ib_desc'];
+
+    pr($ib_url);
+}
