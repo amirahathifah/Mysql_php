f03f46d48606f55b4d0694814a5d5a4422dd7ea6
Amyra Athifah <74502882+amirahathifah@users.noreply.github.com>
2023-01-13 15:39:15 +0800

    Update array2.php

--- view/array2.php
+++ view/array2.php
@@ -8,18 +8,11 @@ Array
             [cl_desc] => Realistic
             [cl_code] => R
         )
-
+    [career] => 1
 )
-$get_construct->result()
+$get_construct->result();
+...
 
--------------------------------------------------------------------------------------
-
-[
-  {"cc_id":"1","cl_id":"1","cl_name":"Realistic","cl_desc":"Realistic","cl_code":"R"}
-]
-json_encode($get_construct->result())
-
---------------------------------------------------------------------------------------
 Array
 (
     [0] => Array
@@ -32,25 +25,23 @@ Array
         )
 
 )
-{
-  "conn_id":
-    {
-      "affected_rows":null,"client_info":null,"client_version":null,"connect_errno":null,"connect_error":null,"errno":null,"error":null,"error_list":null,"field_count":null,"host_info":null,"info":null,"insert_id":null,"server_info":null,"server_version":null,"sqlstate":null,"protocol_version":null,"thread_id":null,"warning_count":null
-    },
-  "result_id":
-    {
-      "current_field":null,"field_count":null,"lengths":null,"num_rows":null,"type":null
-    },
-  "result_array":[],
-  "result_object":
-    [{
-      "cc_id":"1","cl_id":"1","cl_name":"Realistic","cl_desc":"Realistic","cl_code":"R"
-    }],
-  "custom_result_object":[],
-  "current_row":0,
-  "num_rows":null,
-  "row_data":null
-}
-json_decode(json_encode($get_construct->result()), True)
+json_decode(json_encode($get_construct->result()), True);
+....
 
--------------------------------------------------------------------------------------------
+[{"cc_id":"1","cl_id":"1","cl_name":"Realistic","cl_desc":"Realistic","cl_code":"R"}]
+json_encode($get_construct->result());
+.....
+
+{"cc_id":"1","cl_id":"1","cl_name":"Realistic","cl_desc":"Realistic","cl_code":"R"}
+json_encode($get_construct->result());
+.....
+
+stdClass Object
+(
+    [cc_id] => 1
+    [cl_id] => 1
+    [cl_name] => Realistic
+    [cl_desc] => Realistic
+    [cl_code] => R
+)
+$res[] = $get_construct->result();
